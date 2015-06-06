6257ccfca34756d0f14193c2b4f12dc0e7cc1f69
lyrs <lyrs@me.com>
2015-06-06 18:39:22 +0300

    onpay kishki

--- app/controllers/spree/checkout_controller2.rb
+++ app/controllers/spree/checkout_controller2.rb
@@ -25,21 +25,17 @@ module Spree
 
     # Updates the order and advances to the next state (when possible.)
     def update
-
       if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
         @order.temporary_address = !params[:save_user_address]
-
         unless @order.next
           flash[:error] = @order.errors.full_messages.join("\n")
           redirect_to checkout_state_path(@order.state) and return
         end
-        Rails.logger.info ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   STAAARRRRRRRGGGGHHHHT!!!"
-
 
         if @order.completed?
-          session[:order_id] = nil
+          @current_order = nil
           flash.notice = Spree.t(:order_processed_successfully)
-          flash[:commerce_tracking] = "nothing special"
+          flash['order_completed'] = true
           redirect_to completion_route
         else
           redirect_to checkout_state_path(@order.state)
@@ -51,67 +47,34 @@ module Spree
 
     private
 
-    # def redirect_to_onpay
-    #   Rails.logger.info "OLOLO!!!! action is redirect_to_onpay"
-    #
-    #   return unless params[:state] == "payment"
-    #   payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
-    #   Rails.logger.info 'OLOLO!!!! action is redirect_to_onpay'
-    #
-    #   if payment_method.kind_of? Spree::Gateway::Onpay
-    #     Rails.logger.info 'OLOLO!!!! kind is Spree::Gateway::Onpay'
-    #
-    #
-    #     # redirect_to true_onpay_path(:gateway_id => payment_method.id, :order_id => @order.id)
-    #     redirect_to '/'
-    #
-    #   end
-    #
-    # end
-
-
-
-
-
-    def onpay_check
-
-      if params[:order] and params[:order][:payments_attributes] and params[:order][:payments_attributes].first[:payment_method_id] == '7'
-      @order = current_order(lock: true)
-      redirect_to spree.cart_path and return unless @order
-
-
-      if params[:state] == "payment" and params[:order]
-        payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
-
-        # if payment_method.kind_of? Spree::Gateway::Onpay
-
-          redirect_to "/onpay/#{payment_method.id}/#{@order.id}"
-          # redirect_to true_onpay_path(:gateway_id => payment_method.id, :order_id => @order.id)
-          # redirect_to '/'
-
-        # end
-      end
-      end
-
+    def unknown_state?
+      (params[:state] && !@order.has_checkout_step?(params[:state])) ||
+        (!params[:state] && !@order.has_checkout_step?(@order.state))
     end
 
-      def ensure_valid_state
+    def insufficient_payment?
+      params[:state] == "confirm" &&
+        @order.payment_required? &&
+        @order.payments.valid.sum(:amount) != @order.total
+    end
 
-        unless skip_state_validation?
-          if (params[:state] && !@order.has_checkout_step?(params[:state])) ||
-             (!params[:state] && !@order.has_checkout_step?(@order.state))
-            @order.state = 'cart'
-            redirect_to checkout_state_path(@order.checkout_steps.first)
-          end
-        end
-
-        # Fix for #4117
-        # If confirmation of payment fails, redirect back to payment screen
-        if params[:state] == "confirm" && @order.payment_required? && @order.payments.valid.empty?
-          flash.keep
-          redirect_to checkout_state_path("payment")
-        end
+    def correct_state
+      if unknown_state?
+        @order.checkout_steps.first
+      elsif insufficient_payment?
+        'payment'
+      else
+        @order.state
       end
+    end
+
+    def ensure_valid_state
+      if @order.state != correct_state && !skip_state_validation?
+        flash.keep
+        @order.state = correct_state
+        redirect_to checkout_state_path(@order.state)
+      end
+    end
 
       # Should be overriden if you have areas of your checkout that don't match
       # up to a step within checkout_steps, such as a registration step
@@ -120,10 +83,23 @@ module Spree
       end
 
       def load_order_with_lock
-
         @order = current_order(lock: true)
         redirect_to spree.cart_path and return unless @order
+      end
 
+      def ensure_valid_state_lock_version
+        if params[:order] && params[:order][:state_lock_version]
+          @order.with_lock do
+            unless @order.state_lock_version == params[:order].delete(:state_lock_version).to_i
+              flash[:error] = Spree.t(:order_already_updated)
+              redirect_to checkout_state_path(@order.state) and return
+            end
+            @order.increment!(:state_lock_version)
+          end
+        end
+      end
+
+      def set_state_if_present
         if params[:state]
           redirect_to checkout_state_path(@order.state) if @order.can_go_to_state?(params[:state]) && !skip_state_validation?
           @order.state = params[:state]
@@ -131,19 +107,16 @@ module Spree
       end
 
       def ensure_checkout_allowed
-
         unless @order.checkout_allowed?
           redirect_to spree.cart_path
         end
       end
 
       def ensure_order_not_completed
-
         redirect_to spree.cart_path if @order.completed?
       end
 
       def ensure_sufficient_stock_lines
-
         if @order.insufficient_stock_lines.present?
           flash[:error] = Spree.t(:inventory_error_flash_for_insufficient_quantity)
           redirect_to spree.cart_path
@@ -151,8 +124,8 @@ module Spree
       end
 
       # Provides a route to redirect after order completion
-      def completion_route
-        spree.order_path(@order)
+      def completion_route(custom_params = nil)
+        spree.order_path(@order, custom_params)
       end
 
       def setup_for_current_state
@@ -160,19 +133,14 @@ module Spree
         send(method_name) if respond_to?(method_name, true)
       end
 
-      # Skip setting ship address if order doesn't have a delivery checkout step
-      # to avoid triggering validations on shipping address
       def before_address
-
-        @order.bill_address ||= Address.default(try_spree_current_user, "bill")
-
-        if @order.checkout_steps.include? "delivery"
-          @order.ship_address ||= Address.default(try_spree_current_user, "ship")
-        end
+        # if the user has a default address, a callback takes care of setting
+        # that; but if he doesn't, we need to build an empty one here
+        @order.bill_address ||= Address.build_default
+        @order.ship_address ||= Address.build_default if @order.checkout_steps.include?('delivery')
       end
 
       def before_delivery
-
         return if params[:order].present?
 
         packages = @order.shipments.map { |s| s.to_package }
@@ -180,7 +148,6 @@ module Spree
       end
 
       def before_payment
-
         if @order.checkout_steps.include? "delivery"
           packages = @order.shipments.map { |s| s.to_package }
           @differentiator = Spree::Stock::Differentiator.new(@order, packages)
@@ -188,27 +155,20 @@ module Spree
             @order.contents.remove(variant, quantity)
           end
         end
+
+        if try_spree_current_user && try_spree_current_user.respond_to?(:payment_sources)
+          @payment_sources = try_spree_current_user.payment_sources
+        end
       end
 
       def rescue_from_spree_gateway_error(exception)
-
         flash.now[:error] = Spree.t(:spree_gateway_error_flash_for_checkout)
         @order.errors.add(:base, exception.message)
         render :edit
       end
 
       def check_authorization
-
-        authorize!(:edit, current_order, session[:access_token])
-      end
-
-      def persist_user_address
-
-        if @order.checkout_steps.include? "address"
-          if @order.address? && try_spree_current_user.respond_to?(:persist_order_address)
-            try_spree_current_user.persist_order_address(@order) if params[:save_user_address]
-          end
-        end
+        authorize!(:edit, current_order, cookies.signed[:guest_token])
       end
   end
 end
